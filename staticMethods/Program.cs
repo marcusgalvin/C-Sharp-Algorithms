04a4c406ba0c03c6ca453de125a72ab36868cdfc
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-02-03 11:46:47 -0500

    static methods and classes

---
+++ staticMethods/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace staticMethods
+{
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            myStaticClass.sayHi("Matt");
+
+        }
+    }
+}
---
+++ staticMethods/myStaticClass.cs
@@ -0,0 +1,10 @@
+namespace staticMethods
+{
+    static class myStaticClass
+    {
+        public static void sayHi(string name)
+        {
+            System.Console.WriteLine("Hello " + name);
+        }
+    }
+}
